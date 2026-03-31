945dcaf9ad9253cee3b4c65088d44faa85287ae7
ankitsingh-projects <ankitsingh123iec@gmail.com>
2026-03-31 19:13:44 +0530

    payment utilis

--- tests/addKart2Payment.spec.js
+++ tests/addKart2Payment.spec.js
@@ -79,18 +79,9 @@ if( await Products.nth(i).locator('b') === product_Name){
 await Products.nth(i).locator("text=Add To Cart").click();
 await page.locator('//button[@routerlink="/dashboard/cart"]').click();
 await page.locator("text=Checkout").click();
-await page.locator('[placeholder="Select Country"]').pressSequentially('ind',{delay:100});
-const dropdown = await page.locator(".ta-results")
-await dropdown.waitFor();
-const optionCount = await dropdown.locator('button').count();
-for(let i=0; i<optionCount; i++){
-const text = await dropdown.locator('button').nth(i).textContent();
-if(text === "India"){
-await dropdown.locator('button').nth(i).click();
-await page.locator('a[class*=action__submit]').click();
+await Payment(page);
 break;
-}
-}
+
 }
 }
 
